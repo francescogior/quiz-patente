f742f7bad68381473de4f5e375dddfbe433e706e
Francesco Giordano <francescogior@gmail.com>
2026-06-02 16:57:16 +0200

    Stabilize question layout

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 importScripts("./data/asset-manifest-sw.js");
 
-const CACHE_NAME = "quiz-patente-ab-v6";
+const CACHE_NAME = "quiz-patente-ab-v7";
 const CORE_ASSETS = [
   "./",
   "./index.html",
