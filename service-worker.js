a07f77c713a00a50df254cab00ec6d6b8e4f176f
Francesco Giordano <francescogior@gmail.com>
2026-06-03 23:30:39 +0200

    Fix no-image question overflow

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 importScripts("./data/asset-manifest-sw.js");
 
-const CACHE_NAME = "quiz-patente-ab-v17";
+const CACHE_NAME = "quiz-patente-ab-v18";
 const CORE_ASSETS = [
   "./",
   "./index.html",
